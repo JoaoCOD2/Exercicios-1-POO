b38cf394db49a4a50248206bf9cb66555f3857db
JoaoCOD2 <joao.github23@gmail.com>
2026-02-26 16:50:47 -0300

    att

--- ProjetoCelular/src/projetocelular/Celular.java
+++ ProjetoCelular/src/projetocelular/Celular.java
@@ -9,23 +9,45 @@ public class Celular {
     boolean ligado;
     
     public void ligar(){
-       ligado = true;
+       if (bateria > 0){
+           ligado = true;
+           System.out.println("Celular ligado!");
+       }else {
+           System.out.println("Carregue o celular.");
+       }
     }
     
     public void desligar(){
         ligado = false;
+        System.out.println("Celular desligado");
     }
     
     public void usarBatria (int consumo){
-        if (bateria >= 0 && bateria <=100){
-            System.out.println("Ligado");
-        }else {
-            System.out.println("Descarregado");
+        if (!ligado){
+            System.out.println("O celular esta delisgado");
+        }
+        
+        if(bateria - consumo < 0){
+            
+        }else{
+            bateria -= consumo;
+        }
+        
+        System.out.println("Bateria atual: " + bateria + "%");
+        
+        if(bateria == 0){
+            
+            System.out.println("Bateria esgotando. Celular desligando...");
+            desligar();
+            
         }
     }
     
+    
     public void status(){
-        
+        System.out.println("Marca " + marca);
+        System.out.println("Bateria " + bateria);
+        System.out.println("Ligado " + ligado);
     }
 }
-//Regras: So pode usar batreia se estiver ligado, nao permite batria negativa.
+//Regras: So pode usar bateria se estiver ligado, nao permite batria negativa.
